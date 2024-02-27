10e63893c8aa5f7b71209d0862fc2366aa35888d
JordanFierro <jordan.fierro@student.ottawa.edu>
2024-02-26 18:28:05 -0600

    did fundamentals

--- Fundamentals1.playground/Contents.swift
+++ Fundamentals1.playground/Contents.swift
@@ -5,13 +5,16 @@ import UIKit
 // Declare a variable score and a constant maxScore. Assign initial values and print them.
 // Hint: https://guides.codepath.com/ios/Understanding-Swift can be a resource to check syntax
 
-// Write your code here
+var score: Int = 32
+let maxScore: Int = 50
+
+print(score, maxScore)
 
 // Exercise #2:
 // Rewrite the following variables and constants with proper naming conventions for Swift (camelCase).
 
-//var Lastname: String
-//let number_of_wheels: Int
+var lastName: String
+let numberOfWheels: Int
 
 // Write your code here
 
@@ -19,23 +22,40 @@ import UIKit
 // Exercise #3:
 // Declare a constant with an explicit type annotation for a Double and assign a value to it.
 
-// Write your code here
+let num: Double = 200
 
 //Exercise #4:
 // Write a function that prints to the console, "Hello, CodePath!" and then call that function. This function will not take in parameters or return a value.
 
-// Write your code here
+func printHello() {
+    print("Hello, CodePath!")
+}
+
+printHello()
 
 
 // Exercise #5: Declare a function with parameters
 // Write a function that takes in someone's current age and returns their age after their next birthday. Then, use this function to update the age variable and print the new age to the console.
 
-// Write your code here
+func nextAge(personAge: Int){
+    var birthday = personAge + 1
+    
+    print(birthday)
+}
 
+nextAge(personAge: 3)
 // Exercise #6: Declare a function that takes two numbers and returns if the sum is even or not
 // Hint: Think of the parameter types and the return type.
 
-// Write your code here
+func evenOrOdd(firstNum: Int, secondNum: Int) -> Bool {
+    if (firstNum+secondNum) % 2 == 0 {
+        return true
+    }
+    else {
+        return false
+    }
+}
 
+evenOrOdd(firstNum: 5, secondNum: 11)
 
 
--- Fundamentals2.playground/Contents.swift
+++ Fundamentals2.playground/Contents.swift
@@ -4,7 +4,15 @@
 //  print it.
 
 // Write your code below:
-
+//var nickname: String?
+//
+//nickname = "Jimmy"
+//
+//if let name = nickname {
+//    print("Nickname: \(name)")
+//} else {
+//    print("No nickname")
+//}
 
 
 
@@ -12,10 +20,43 @@
 // Exercise #2: Create a class `Person` with an optional property `nickname`. Then instantiate a `Person` and use optional chaining to print the nickname if it exists.
 
 // Write your code below:
+class Person {
+    static var numberOfPeople = 0
+    
+    init(firstName: String, lastName: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+        
+        Person.numberOfPeople += 1
+    }
+    
+    var firstName: String
+    var lastName: String
+    var nickname: String?
+    
+    var fullName: String {
+        get {
+            "\(firstName) \(lastName)"
+        }
+    }
+            
+    class func printNumberOfPeople (){
+        print("Number of people: \(Person.numberOfPeople)")
+    }
+}
 
+let jim = Person(firstName: "Jim", lastName: "Brown")
+jim.nickname = "Jimmy"
 
+if let name = jim.nickname {
+    print("Nickname is \(name)")
+} else {
+    print("No nickname")
+}
 
+print("Jim's full name is \(jim.fullName)")
 
+Person.printNumberOfPeople()
 
 // Exercise #3: Implicitly Unwrapped Optionals
 // An implicitly unwrapped optional is a type of optional that you assume will always have a value after it's first set.
@@ -25,21 +66,68 @@
 
 // Write your code below:
 
+class Dog {
+    var name: String
+    var nickname: String!  // Implicitly unwrapped optional
 
+    init(name: String, nickname: String? = nil){
+        self.name = name
+        self.nickname = nickname
+    }
+}
 
+let dogs = [Dog(name: "dog"), Dog(name: "Sally", nickname: "sam"), Dog(name: "Panda", nickname: "Panther"), Dog(name: "yo")]
 
+for dog in dogs {
+    print("\(dog.name) goes by \(dog.nickname ?? dog.name)")
+}
 
 
 // Exercise #4: Type Casting w/ Optionals
 //  Create an array of different types (Int, String, Double) as [Any], then write a function that iterates over the array and prints the type and value of each element using optional casting.
 
 // Write your code below:
+let mixedArray: [Any] = [42, "pie", 3.14, "string", 4242.44, 57]
 
+func printTypesAndValues(from array: [Any]) {
+    for element in array {
+        if let intValue = element as? Int {
+            print("Int: \(intValue)")
+        } else if let stringValue = element as? String {
+            print("String: \(stringValue)")
+        } else if let doubleValue = element as? Double {
+            print("Double: \(doubleValue)")
+        } else {
+            print("Unknown value")
+        }
+    }
+}
 
-
+printTypesAndValues(from: mixedArray)
 // Exercise #5: Advanced Optional Handling with guard
 // Create a Cat class that includes a method that uses 'guard let' to safely unwrap the nickname
 
 // Write your code below:
+class Cat {
+    var name: String
+    var nickname: String?
+    
+    init(name: String, nickname: String? = nil) {
+        self.name = name
+        self.nickname = nickname
+    }
+    
+    func printNicknames() {
+        guard let nickname = self.nickname else {
+            print("\(self.name) does not have a nickname.")
+            return
+        }
+        print("\(self.name) nickname is \(nickname)")
+    }
+}
 
+let cats = [Cat(name: "Whiskers"), Cat(name: "Buddy", nickname: "Cum guzzle")]
 
+for cat in cats {
+    cat.printNicknames()
+}
